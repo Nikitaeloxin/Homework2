6eb2eedc4e70b834de401c065d74708d5606cd02
Nikitaeloxin <nikitaeloxin@yandex.ru>
2021-11-14 12:31:27 +0200

    add age to schedules

--- src/Schedules.java
+++ src/Schedules.java
@@ -2,6 +2,7 @@ public class Schedules {
     
     private String teacherName;
     private String topic;
+    private int age;
     Schedules(String n, String t)  {
         teacherName = n;
         topic = t;
